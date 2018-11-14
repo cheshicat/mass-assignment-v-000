18e475e46f5aa8c418366d790b64245ad44bd961
Allison Howard <alliemhow@gmail.com>
2018-11-14 13:47:45 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,5 @@
 class Person
-  attr_accessor :name, :birthday, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size,
+  attr_accessor :name, :birthday, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size,
 
   def initialize(attributes)
   end
